431638c4e317cb36a341d47ea1f53b89f09719ca
JerryHu <31403436@qq.com>
2017-07-23 13:50:45 +0800

    add pm sql

---
+++ db/pm/pm.sql
@@ -0,0 +1,41 @@
+/*
+SQLyog 企业版 - MySQL GUI v7.14 
+MySQL - 5.7.19-log : Database - jeesite
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`jeesite` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
+
+USE `jeesite`;
+
+/*Table structure for table `pm_tree` */
+
+DROP TABLE IF EXISTS `pm_tree`;
+
+CREATE TABLE `pm_tree` (
+  `id` varchar(64) COLLATE utf8_bin NOT NULL COMMENT '编号',
+  `parent_id` varchar(64) COLLATE utf8_bin NOT NULL COMMENT '父级编号',
+  `parent_ids` varchar(2000) COLLATE utf8_bin NOT NULL COMMENT '所有父级编号',
+  `name` varchar(100) COLLATE utf8_bin NOT NULL COMMENT '名称',
+  `sort` decimal(10,0) NOT NULL COMMENT '排序',
+  `create_by` varchar(64) COLLATE utf8_bin NOT NULL COMMENT '创建者',
+  `create_date` datetime NOT NULL COMMENT '创建时间',
+  `update_by` varchar(64) COLLATE utf8_bin NOT NULL COMMENT '更新者',
+  `update_date` datetime NOT NULL COMMENT '更新时间',
+  `remarks` varchar(255) COLLATE utf8_bin DEFAULT NULL COMMENT '备注信息',
+  `del_flag` char(1) COLLATE utf8_bin NOT NULL DEFAULT '0' COMMENT '删除标记',
+  `files` varchar(2000) COLLATE utf8_bin DEFAULT NULL COMMENT '附件',
+  PRIMARY KEY (`id`),
+  KEY `test_tree_del_flag` (`del_flag`),
+  KEY `test_data_parent_id` (`parent_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='项目树结构表';
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
